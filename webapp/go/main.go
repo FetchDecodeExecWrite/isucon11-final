8024f6376b46ee8b25515842cb2d0bcb6fb94cd2
akouryy <akouryy7@gmail.com>
2021-09-18 11:04:45 +0900

    Revert "課題提出で tx.Commit をファイル書き込みより先にする"
    This reverts commit 87afc43b14ce612100b40e95e57127d04bc0da62.

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -1184,11 +1184,6 @@ func (h *handlers) SubmitAssignment(c echo.Context) error {
 		return c.NoContent(http.StatusInternalServerError)
 	}
 
-	if err := tx.Commit(); err != nil {
-		c.Logger().Error(err)
-		return c.NoContent(http.StatusInternalServerError)
-	}
-
 	data, err := io.ReadAll(file)
 	if err != nil {
 		c.Logger().Error(err)
@@ -1197,6 +1192,11 @@ func (h *handlers) SubmitAssignment(c echo.Context) error {
 
 	dst := AssignmentsDirectory + classID + "-" + userID + ".pdf"
 	if err := os.WriteFile(dst, data, 0666); err != nil {
+		c.Logger().Error(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	if err := tx.Commit(); err != nil {
 		c.Logger().Error(err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
